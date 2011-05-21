4d6eb18a281bf4693281367e4e5a2de14b84633c
Zemian Deng <Zemian Deng@localhost>
2011-05-21 00:10:50 -0400

    for quartz 1.8 exp updated run-java clean up spring xml

--- quartz-1.8-experiment/bin/run-quartz-dev.sh
+++ quartz-1.8-experiment/bin/run-quartz-dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\test-classes"
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\classes"
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\dependency\*"
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\test-classes"
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\classes"
-JAVA_CP="$JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\dependency\*"
-export JAVA_CP
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\test-classes"
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\classes"
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz\target\dependency\*"
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\test-classes"
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\classes"
+RUN_JAVA_CP="$RUN_JAVA_CP;C:\projects\quartz-1.8.x-branch\quartz-oracle\target\dependency\*"
+export RUN_JAVA_CP
 DIR=$(dirname $0)
 $DIR/run-java quartz.experiment.QuartzServer -Dconfig=$1
