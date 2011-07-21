c60ca4c59c86918614315811471961247590763b
Zemian Deng <Zemian Deng@localhost>
2011-07-20 22:33:49 -0400

    added spring305-quartz project

---
+++ spring305-quartz/src/main/test/quartz/experiment/QuickTest.java
@@ -0,0 +1,23 @@
+package quartz.experiment;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class QuickTest {
+	@Autowired
+	protected Scheduler scheduler;
+	
+	@Test 
+	public void testSpringContext() throws Exception {
+		assertThat(scheduler.getSchedulerName(), is("SpringQuartzScheduler"));
+	}
+}
